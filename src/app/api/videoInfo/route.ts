ab8de0f0f70f8a0ac80d6587a0249a17e06c430c
墨綠BG <s5460703@gmail.com>
2024-05-29 11:54:16 +0800

    🐞 fix：修复属性丢失

--- src/app/api/videoInfo/route.ts
+++ src/app/api/videoInfo/route.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 
-// 假设这是从服务器端API获取的B站视频信息
-const userCookie =
-  "buvid3=4528212C-2E8E-43D3-7771-08E3D097016B43101infoc; b_nut=1697991243; _uuid=8F3109D6E-37F6-109BE-EE59-CFF43586727A44186infoc; buvid4=FB8AB4CA-5AE3-F9AE-B971-59CA3110583644147-023102300-sc6TKcUNTk72ytH9fgLXOA%3D%3D; rpdid=|(JY~|lk~)u~0J'uYm~mmukRR; enable_web_push=DISABLE; header_theme_version=CLOSE; DedeUserID=21907943; DedeUserID__ckMd5=472ff340a3ce3940; buvid_fp_plain=undefined; CURRENT_BLACKGAP=0; CURRENT_FNVAL=4048; hit-dyn-v2=1; PVID=1;";
+// const userCookie =
+//   "buvid3=4528212C-2E8E-43D3-7771-08E3D097016B43101infoc; b_nut=1697991243; _uuid=8F3109D6E-37F6-109BE-EE59-CFF43586727A44186infoc; buvid4=FB8AB4CA-5AE3-F9AE-B971-59CA3110583644147-023102300-sc6TKcUNTk72ytH9fgLXOA%3D%3D; rpdid=|(JY~|lk~)u~0J'uYm~mmukRR; enable_web_push=DISABLE; header_theme_version=CLOSE; DedeUserID=21907943; DedeUserID__ckMd5=472ff340a3ce3940; buvid_fp_plain=undefined; CURRENT_BLACKGAP=0; CURRENT_FNVAL=4048; hit-dyn-v2=1; PVID=1;";
 const infoApi = 'https://api.bilibili.com/x/web-interface/view?bvid=';
 
 export async function POST(req: Request, res: Response) {
@@ -14,7 +13,7 @@ export async function POST(req: Request, res: Response) {
       'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0',
       Referer: 'https://www.bilibili.com/',
-      Cookie: userCookie,
+      // Cookie: userCookie,
     };
 
     const response = await axios.get(infoApi + bvid, { headers });
