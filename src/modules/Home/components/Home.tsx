70a667860e2b9ba330fb25e852f8ff409dc53793
墨綠BG <s5460703@gmail.com>
2026-06-10 01:11:40 +0800

    🐛 fix(ssr): lazy load Search via next/dynamic with ssr: false
    Antd 6 and pro-components 3 beta touch window during module
    evaluation, which crashed Next 15 SSG prerender for / and /search.
    Loading the Search module client-side restores static prerender
    and shrinks First Load JS from 442 kB to 104 kB.

--- src/modules/Home/components/Home.tsx
+++ src/modules/Home/components/Home.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import dynamic from 'next/dynamic';
 import React from 'react';
 
-import Search from '@/modules/Search';
+const Search = dynamic(() => import('@/modules/Search'), {
+  ssr: false,
+  loading: () => null,
+});
 
 const Home: React.FC = React.memo(() => <Search />);
 
